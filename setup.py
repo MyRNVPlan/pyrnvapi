150586de7055bfdc3404f3f806bec2eca6499176
Alexander Stepanov <keks@al3xst.de>
2019-12-27 17:26:07 +0100

    PEP8 in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name="pyrnvapi",
-      version="0.2",
+      version="0.2.1",
       description="Client API for the official REST API provied by the RNV GmbH",
       url="http://github.com/myrnvplan/pyrnvapi",
-      author = "Alexander Stepanov",
-      author_email = "keks+github@al3xst.de",
-      license = "MIT",
-      packages = ["pyrnvapi"],
+      author="Alexander Stepanov",
+      author_email="keks+github@al3xst.de",
+      license="MIT",
+      packages=["pyrnvapi"],
       install_requires=["requests"])
